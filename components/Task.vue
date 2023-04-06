667282c9418889510dff71e65062745ee9315b59
ibraheem mariam <ibraheemmaryam26@gmail.com>
2023-04-06 20:38:45 +0100

    commit

--- components/Task.vue
+++ components/Task.vue
@@ -1,25 +1,33 @@
 <template>
-  <div :class= "`task ${task.done ? 'is-complete': ''}`">
-       <div class="content">{{task.content}}</div>
-       <div class="buttons">
-            <button @click="toggleDone">{{task.done ? 'Undo' : 'Done' }}</button>
-            <button @click="removeTask" class="delete">Delete</button>
-       </div>
-
+  <div :class="`task ${task.done ? 'is-complete': ''}`">
+    <div class="content">{{task.content}}</div>
+    <div class="buttons">
+      <button @click="toggleDone">{{task.done ? 'Undo' : 'Done' }}</button>
+      <button @click="removeTask" class="delete">Delete</button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-    props:['task'],
-    methods:{
-        toggleDone() {
-            this.$store.commit('TOGGLE_TASK',this.task);
-        },
-        removeTask(){
-            this.$store.commit('REMOVE_TASK',this.task);
-        }
+  props: {
+    task: {
+      type: Object,
+      default: () => ({})
     }
+  },
+  created() {
+    console.log('Task component created')
+    
+  },
+  methods: {
+    toggleDone() {
+      this.$store.commit('TOGGLE_TASK', this.task)
+    },
+    removeTask() {
+      this.$store.commit('REMOVE_TASK', this.task)
+    }
+  }
 }
 </script>
 
